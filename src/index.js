3c47151fb026c530c584b17c33a2123b4ee4e2f9
Abdelrahmanbadr3 <body.badr1510@gmail.com>
2021-07-15 03:38:49 +0200

    add current user to firestore and redux

--- src/index.js
+++ src/index.js
@@ -7,14 +7,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import { createFirestoreInstance } from "redux-firestore";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import * as config from "./config";
-
 import store from "./store";
 
 firebase.initializeApp(config.fbConfig);
