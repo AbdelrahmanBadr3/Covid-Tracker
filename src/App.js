d60ad077abcc6b2c3bf40e31caf39fe3d7ca7a76
Abdelrahman Badr <46943945+AbdelrahmanBadr3@users.noreply.github.com>
2021-07-25 15:07:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { fetchUserLocation } from "./features/user/userSlice";
 import { useState, useEffect } from "react";
 import Dashboard from "./Dashboard";
 import UserForm from "./UserForm";
-import Loading from "./Loading";
+// import Loading from "./Loading";
 function App() {
   const dispatch = useDispatch();
   const { currentUserLocation, currentUser } = useSelector(
@@ -14,7 +14,7 @@ function App() {
   );
   const [navValue, setNavValue] = useState(1);
   const [currentPage, setCurrentPage] = useState(<Dashboard />);
-  const [loadedPage, setLoadedPage] = useState(<Loading />);
+//   const [loadedPage, setLoadedPage] = useState(<Loading />);
 
   useEffect(() => {
     dispatch(fetchUserLocation());
@@ -27,10 +27,50 @@ function App() {
     currentUser,
   ]);
 
-  useEffect(() => {
-    if (currentUserLocation != null && currentUserLocation.address!="") {
-      setLoadedPage(
-        <div>
+//   useEffect(() => {
+//     if (currentUserLocation != null && currentUserLocation.address!="") {
+//       setLoadedPage(
+//         <div>
+//           <Nav
+//             justify
+//             fill
+//             variant="tabs"
+//             defaultActiveKey={navValue}
+//             onSelect={onSelect}
+//           >
+//             <Nav.Item>
+//               <Nav.Link eventKey={1}>Dashboard</Nav.Link>
+//             </Nav.Item>
+//             <Nav.Item>
+//               <Nav.Link eventKey={2}>
+//                 User Information{" "}
+//                 <Badge variant={currentUser.userState.variant}>
+//                   {currentUser.userState.type}
+//                 </Badge>
+//               </Nav.Link>
+//             </Nav.Item>
+//           </Nav>
+//           {currentPage}
+//         </div>
+//       );
+//     }
+//   }, [
+//     currentUserLocation,
+//     currentPage,
+//     currentUser.userState.type,
+//     currentUser.userState.variant,
+//     navValue,
+//   ]);
+
+  const onSelect = (key) => {
+    if (key === "1") setCurrentPage(<Dashboard />);
+    else setCurrentPage(<UserForm />);
+    setNavValue(key);
+  };
+
+  return <div className="App">
+//     {loadedPage}
+     <div>
           <Nav
             justify
             fill
@@ -52,23 +92,7 @@ function App() {
           </Nav>
           {currentPage}
         </div>
-      );
-    }
-  }, [
-    currentUserLocation,
-    currentPage,
-    currentUser.userState.type,
-    currentUser.userState.variant,
-    navValue,
-  ]);
-
-  const onSelect = (key) => {
-    if (key === "1") setCurrentPage(<Dashboard />);
-    else setCurrentPage(<UserForm />);
-    setNavValue(key);
-  };
-
-  return <div className="App">{loadedPage}</div>;
+  </div>;
 }
 
 export default App;
