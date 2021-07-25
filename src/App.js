0dbd804bfe50d497c8338e4d7f451e54f166fe1b
Abdelrahman Badr <46943945+AbdelrahmanBadr3@users.noreply.github.com>
2021-07-25 17:31:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import { fetchUserLocation } from "./features/user/userSlice";
 import { useState, useEffect } from "react";
 import Dashboard from "./Dashboard";
 import UserForm from "./UserForm";
-// TODO To add loading page uncommet the following lines
-// import Loading from "./Loading";
+import Loading from "./Loading";
 function App() {
   const dispatch = useDispatch();
   const { currentUserLocation, currentUser } = useSelector(
@@ -15,8 +14,7 @@ function App() {
   );
   const [navValue, setNavValue] = useState(1);
   const [currentPage, setCurrentPage] = useState(<Dashboard />);
-  // TODO To add loading page un commet the following lines                                               
-//   const [loadedPage, setLoadedPage] = useState(<Loading />);
+  const [loadedPage, setLoadedPage] = useState(<Loading />);
 
   useEffect(() => {
     dispatch(fetchUserLocation());
@@ -28,55 +26,10 @@ function App() {
     currentUserLocation,
     currentUser,
   ]);
-// TODO To add loading page uncommet the following lines
-//   useEffect(() => {
-//     if (currentUserLocation != null && currentUserLocation.address!="") {
-//       setLoadedPage(
-//         <div>
-//           <Nav
-//             justify
-//             fill
-//             variant="tabs"
-//             defaultActiveKey={navValue}
-//             onSelect={onSelect}
-//           >
-//             <Nav.Item>
-//               <Nav.Link eventKey={1}>Dashboard</Nav.Link>
-//             </Nav.Item>
-//             <Nav.Item>
-//               <Nav.Link eventKey={2}>
-//                 User Information{" "}
-//                 <Badge variant={currentUser.userState.variant}>
-//                   {currentUser.userState.type}
-//                 </Badge>
-//               </Nav.Link>
-//             </Nav.Item>
-//           </Nav>
-//           {currentPage}
-//         </div>
-//       );
-//     }
-//   }, [
-//     currentUserLocation,
-//     currentPage,
-//     currentUser.userState.type,
-//     currentUser.userState.variant,
-//     navValue,
-//   ]);
-
-  const onSelect = (key) => {
-    if (key === "1") setCurrentPage(<Dashboard />);
-    else setCurrentPage(<UserForm />);
-    setNavValue(key);
-  };
-
-  return <div className="App">
-   
-     {/* TODO To add loading page uncommet the following lines
-     loadedPage
-      TODO To add loading page commet the following lines
-     */}
-     <div>
+  useEffect(() => {
+    if (currentUserLocation != null && currentUserLocation.address!="") {
+      setLoadedPage(
+        <div>
           <Nav
             justify
             fill
@@ -98,6 +51,24 @@ function App() {
           </Nav>
           {currentPage}
         </div>
+      );
+    }
+  }, [
+    currentUserLocation,
+    currentPage,
+    currentUser.userState.type,
+    currentUser.userState.variant,
+    navValue,
+  ]);
+
+  const onSelect = (key) => {
+    if (key === "1") setCurrentPage(<Dashboard />);
+    else setCurrentPage(<UserForm />);
+    setNavValue(key);
+  };
+
+  return <div className="App"> { loadedPage }
+
   </div>;
 }
 
